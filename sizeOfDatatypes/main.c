a10eb6cbad8eb62a43f6579ede3bcd120c05f1ee
y2kun <yuma.fellinger@gmail.com>
2023-05-17 13:52:32 +0200

    Inaugural Commit

--- sizeOfDatatypes/main.c
+++ sizeOfDatatypes/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <malloc.h>
+#include <inttypes.h>
 
 int main() {
+    int size = sizeof(int64_t);
     int *ptr = malloc(sizeof(int) * 10);
-    printf("%x", *ptr);
-
+    printf("%i\n", size);
+    printf("%x\n", *ptr);
     return 0;
 }
